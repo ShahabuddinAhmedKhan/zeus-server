6f9484c51f9187c43acad0284f8d4bd28868fb8a
Zeusine <shawon2012.sk@gmail.com>
2025-07-11 15:28:39 +0600

    delete api of my equpment page implemented

--- index.js
+++ index.js
@@ -104,6 +104,12 @@ async function run() {
             res.send(result)
         })
 
+        app.delete(`/myEquipment/:id`, async (req, res) => {
+            const id = req.params.id
+            const query = {_id : new ObjectId(id)}
+            const result =  await equipmentCollection.deleteOne(query)
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
